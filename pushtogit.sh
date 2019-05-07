0198b263433905ed0f9deb4a36905138b21e104c
nvdinh185 <nvdinh0905671805@gmail.com>
2019-05-07 13:43:23 +0700

    commit to upload file

--- pushtogit.sh
+++ pushtogit.sh
@@ -1,5 +1,5 @@
-git init
+#git init
 git add .
 git commit -m "commit to upload file"
-git remote add origin https://github.com/nvdinh185/ionic-upload-file.git
+#git remote add origin https://github.com/nvdinh185/ionic-upload-file.git
 git push -u origin master
