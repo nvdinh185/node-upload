5e7b674f1c1b4209641c794af4b2fb86fe2c4a3d
nvdinh185 <nvdinh0905671805@gmail.com>
2019-09-26 09:20:56 +0700

    commit

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
 var http = require('http');
-var fs = require('fs');
 var formidable = require('formidable');
+var fs = require('fs');
 
-http.createServer(function (req, res) {
-    //Nếu request là uplooad và method là post
-    if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
-        //Khởi tạo form
-        var form =  new formidable.IncomingForm();
-        //Thiết lập thư mục chứa file trên server
-        form.uploadDir = "uploads/";
-        //xử lý upload
-        form.parse(req,function (err, fields, file) {
-            //path tmp trên server
-            var path = file.files.path;
-            //thiết lập path mới cho file
-            var newpath = form.uploadDir + file.files.name;
-            fs.rename(path, newpath, function (err) {
-                if (err) throw err;
-                res.end('Upload Thanh cong!');
-            });
-        });
-        return ;
-    }
-    //xét header cho request
-    res.writeHead('200',{'Content-Type': 'text/html'});
-    //Đọc file index và trả về dữ liệu
-    fs.readFile('index.html','utf8',function (err,data) {
-        //nếu nỗi thì thông báo
+http.createServer((req, res) => {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    //thư mục chứa files upload lên
+    form.uploadDir = "uploads/";
+    //Chép file đã chọn vào bộ nhớ tạm "C:\Users\DinhNV\AppData\Local\Temp\"
+    form.parse(req, (err, fields, files) => {
+      //đường dẫn lưu file tạm thời
+      var oldpath = files.filetoupload.path; //C:\Users\DinhNV\AppData\Local\Temp\upload_057f6ec8fe1c25a7b3a954174cfc51dc
+      //đường dẫn thực muốn lưu file
+      var newpath = form.uploadDir + files.filetoupload.name;
+      //di chuyển file từ đường dẫn tạm sang đường dẫn thực
+      fs.rename(oldpath, newpath, err => {
         if (err) throw err;
-        //không lỗi thì render data
-        res.end(data);    
-    })
-}).listen(8000);
+        res.end('File uploaded!');
+      });
+    });
+    return;
+  }
+
+  //Render ra giao diện upload files
+  res.writeHead('200', { 'Content-Type': 'text/html' });
+  fs.readFile('index.html', 'utf8', (err, data) => {
+    if (err) throw err;
+    res.end(data);
+  })
+
+}).listen(8080, () => console.log("Server is running..."));
