b4929673a3c2e15e89ce7fd7d2f051514b73b891
nvdinh185 <nvdinh0766777123@gmail.com>
2022-06-09 14:34:40 +0700

    uploadfile only

--- server.js
+++ server.js
@@ -2,49 +2,45 @@ const http = require('http');
 const formidable = require('formidable');
 const fs = require('fs');
 
-const Excel = require('exceljs');
-const wb = new Excel.Workbook();
 const path = require('path');
-
-const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/";
 
 http.createServer((req, res) => {
   if (req.url == '/fileupload') {
     const form = new formidable.IncomingForm();
     //thư mục chứa files upload lên
-    form.uploadDir = "uploads/";
-    //Chép file đã chọn vào bộ nhớ tạm "C:\Users\DinhNV\AppData\Local\Temp\"
+    const dirUpload = "uploads/";
+    form.uploadDir = dirUpload;
+    const formData = { params: {}, files: {} };
+
+    //Tạo thư mục chứa file upload
+    if (!fs.existsSync(dirUpload)) {
+      fs.mkdirSync(dirUpload);
+    }
+    //Chép file đã chọn vào nhưng chưa đổi tên
     form.parse(req, (err, fields, files) => {
-      //đường dẫn lưu file tạm thời
-      const oldpath = files.filetoupload.path; //C:\Users\DinhNV\AppData\Local\Temp\upload_057f6ec8fe1c25a7b3a954174cfc51dc
-      //đường dẫn thực muốn lưu file
+      for (const key in fields) {
+        Object.defineProperty(formData.params, key, {
+          value: fields[key],
+          enumerable: true //cho phep gan thanh thuoc tinh truy van sau khi hoan thanh
+        });
+      }
+      //tên file tạm thời
+      const oldpath = files.filetoupload.path; //upload_057f6ec8fe1c25a7b3a954174cfc51dc
+      //tên file mới
       const newpath = form.uploadDir + files.filetoupload.name;
-      //di chuyển file từ đường dẫn tạm sang đường dẫn thực
+      //đổi tên file
       fs.rename(oldpath, newpath, err => {
         if (err) throw err;
-        // Tiến hành đọc file excel đã upload lên
+        // in ra đường dẫn file đã upload lên
         const filePath = path.resolve(__dirname, newpath);
-        wb.xlsx.readFile(filePath).then(() => {
-          const sh = wb.getWorksheet("phone-number");
-          const arData = [];
-          sh.eachRow((row, rowNumber) => {
-            if (rowNumber >= 2) {
-              let jsonData = { id: row.getCell(1).value, phone: row.getCell(2).value };
-              arData.push(jsonData);
-            }
-          });
-          MongoClient.connect(url, (err, db) => {
-            if (err) throw err;
-            const dbo = db.db("phone-number");
-            dbo.collection("users").insertMany(arData, function (err, res) {
-              if (err) throw err;
-              console.log("Number of documents inserted: " + res.insertedCount);
-              db.close();
-            });
-          });
-          // console.log(arData);
-        }).catch(err => console.log("Lỗi: ", err));
+        const key = "file";
+        Object.defineProperty(formData.files, key, {
+          value: {
+            url: filePath
+          },
+          enumerable: true //cho phep gan thanh thuoc tinh truy van sau khi hoan thanh
+        });
+        console.log(formData);
         res.end('File uploaded!');
       });
     });
